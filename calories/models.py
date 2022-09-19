154e153652b6c88f4bbf398c18252ccd5312de76
asandelarvine <asandelarvine@gmail.com>
2022-09-19 11:07:36 +0300

    profile model is the main model that stores all the required data of the user for tracking calories.

--- calories/models.py
+++ calories/models.py
@@ -5,7 +5,7 @@ from datetime import date
 
 
 # Create your models here.
-
+# food model
 class Food(models.Model):
 	name = models.CharField(max_length=200 ,null=False)
 	quantity = models.PositiveIntegerField(null=False,default=0)
@@ -14,3 +14,36 @@ class Food(models.Model):
 
 	def __str__(self):
 		return self.name
+
+# profile model
+class Profile(models.Model):
+	person_of = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+	calorie_count = models.FloatField(default=0,null=True,blank=True)
+	food_selected = models.ForeignKey(Food,on_delete=models.CASCADE,null=True,blank=True)
+	quantity = models.FloatField(default=0)
+	total_calorie = models.FloatField(default=0,null=True)
+	date = models.DateField(auto_now_add = True)
+	calorie_goal = models.PositiveIntegerField(default=0)
+	all_food_selected_today = models.ManyToManyField(Food,through='PostFood',related_name='inventory')
+
+	
+
+	
+	def save(self, *args, **kwargs):# new
+		if self.food_selected != None:
+			self.amount = (self.food_selected.calorie/self.food_selected.quantity) 
+			self.calorie_count = self.amount*self.quantity
+			self.total_calorie = self.calorie_count + self.total_calorie  
+			calories = Profile.objects.filter(person_of=self.person_of).last()
+			PostFood.objects.create(profile=calories,food=self.food_selected,calorie_amount=self.calorie_count,amount=self.quantity)
+			self.food_selected = None
+			super(Profile, self).save(*args,**kwargs)
+	
+		else:
+			super(Profile,self).save(*args,**kwargs)
+
+
+
+	def __str__(self):
+		return str(self.person_of.username)
+        
