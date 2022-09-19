ff2c1a66cb838aadb10011884c602d25be2a238d
asandelarvine <asandelarvine@gmail.com>
2022-09-19 19:46:22 +0300

    views for registering users,login page and logging out page

--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -44,3 +44,43 @@ def HomePageView(request):
 	
 
 	return render(request, 'home.html',context)
+
+#signup page
+def RegisterPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request,"Account was created for "+ user)
+				return redirect('login')
+
+		context = {'form':form}
+		return render(request,'register.html',context)
+
+#login page
+def LoginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user = authenticate(request,username=username,password=password)
+			if user is not None:
+				login(request,user)
+				return redirect('home')
+			else:
+				messages.info(request,'Username or password is incorrect')
+		context = {}
+		return render(request,'login.html',context)
+
+#logout page
+def LogOutPage(request):
+	logout(request)
+	return redirect('login')
